6749225c6132928f6aeacdbc5b780aad9560bbe1
dhruv35m <dhruv35m@gmail.com>
2023-10-25 17:24:23 +0530

    basic config

---
+++ lua/user/config/autocmds.lua
@@ -0,0 +1,11 @@
+-- vim.api.nvim_create_autocmd('BufEnter', {
+--   pattern = {
+--     "*.lua",
+--     "*.js",
+--     "*.ts",
+--   },
+--   callback = function ()
+--     vim.opt_local.ts = 2
+--     vim.opt_local.sw = 2
+--   end
+-- })
